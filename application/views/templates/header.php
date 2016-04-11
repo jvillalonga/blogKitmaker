3d9f2ebe88ace2be62be5984876294eb55cbb2ff
jvillalonga <jvillalonga@kitmaker.com>
2016-04-11 13:06:18 +0200

    añadido jqery
    efectos de jquery añadidos

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,6 +13,14 @@
 
             $(document).ready(function () {
                 $('#taula').DataTable();
+                
+                $('.article').hide();
+                $('.comment').hide();
+                $('#deviceInfo').hide();
+                
+                $('.article').slideDown();
+                $('.comment').slideDown();
+                $('#deviceInfo').slideDown();
             });
             
         </script>
