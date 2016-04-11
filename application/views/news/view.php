6a9dfbcbff2d5195412a4f1417542c1322836167
jvillalonga <jvillalonga@kitmaker.com>
2016-04-11 12:42:19 +0200

    4 arreglos
    ds

--- application/views/news/view.php
+++ application/views/news/view.php
@@ -1,6 +1,6 @@
 <div class="article">
     <?php echo '<h2>' . $news_item['title'] . '</h2>'; ?>
-    <p class="fecha"><?php echo $news_item['fecha']?>, por <?php echo $news_item['autor']; ?></p>
+    <p class="infoArt"><?php echo $news_item['fecha']?>, por <?php echo $news_item['autor']; ?></p>
     <?php echo $news_item['text']; ?>
 
     <p><a href="<?php echo site_url('news/') ?>">Volver</a></p>
